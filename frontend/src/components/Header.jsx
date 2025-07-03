00110195465aec0e206d12695d0f3e04cbd03b1d
Prem <tinkushakya2003@gmail.com>
2025-07-03 15:01:41 +0530

    add

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -41,7 +41,7 @@ const Header = () => {
       // }
 
       Cookie.remove("token");
-     navigate("/")
+      window.location.href = "/";
       toast.success("User loggedOut");
     } catch (error) {
       toast.error("Error logging out");
